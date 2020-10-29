f974c165b948b580a38626cfc0e81b4cb4518099
DJ Steinmetz <dsteinmetz@four51.com>
2020-10-29 15:15:41 -0500

    Add two more suppliers

--- src/headstart/supplier-product-xp-Facet.ts
+++ src/headstart/supplier-product-xp-Facet.ts
@@ -130,6 +130,70 @@ async function runImpactCanopyCanada() {
     console.log("done")
 }
 
+async function runLifeFitness() {
+    const creds = config.prod.SEB.LifeFitness;
+    const sdk = await helpers.ocClient(creds.clientID, creds.clientSecret, 'Production');
+    const patch = { xp: { Facets: { supplier: ['Life Fitness']} } };
+
+    console.log("getting products...")
+    const products = await helpers.listAll<Product>(sdk.Products.List);
+    console.log("Got all products")
+
+    const total = products.length;
+    let progress = 0;
+    const errors = {};
+
+    console.log(`Patching ${total} products.`)
+
+    await helpers.batchOperations(products, async function singleOperation(
+        product: Product
+    ): Promise<any> {
+        try {
+            await sdk.Products.Patch(product.ID!, patch);
+            console.log(`Patched ${progress} out of ${total}`);
+            progress++;
+        } catch (e) {
+            console.log("error")
+            errors[product.ID!] = e;
+        }
+    })
+    await helpers.log(errors)
+    helpers.log(errors, 'SEB-patch-errors');
+    console.log("done")
+}
+
+async function runTorqueFitness() {
+    const creds = config.prod.SEB.TorqueFitness;
+    const sdk = await helpers.ocClient(creds.clientID, creds.clientSecret, 'Production');
+    const patch = { xp: { Facets: { supplier: ['Torque Fitness']} } };
+
+    console.log("getting products...")
+    const products = await helpers.listAll<Product>(sdk.Products.List);
+    console.log("Got all products")
+
+    const total = products.length;
+    let progress = 0;
+    const errors = {};
+
+    console.log(`Patching ${total} products.`)
+
+    await helpers.batchOperations(products, async function singleOperation(
+        product: Product
+    ): Promise<any> {
+        try {
+            await sdk.Products.Patch(product.ID!, patch);
+            console.log(`Patched ${progress} out of ${total}`);
+            progress++;
+        } catch (e) {
+            console.log("error")
+            errors[product.ID!] = e;
+        }
+    })
+    await helpers.log(errors)
+    helpers.log(errors, 'SEB-patch-errors');
+    console.log("done")
+}
+
 // invoke the proper function here, then debug in the debug panel
 // TODO: Make sure that you add 'ProductAdmin' to the roles array in the helpers.ocClient function
 // in order to avoid authentication errors.  Suppliers do not have full access.
