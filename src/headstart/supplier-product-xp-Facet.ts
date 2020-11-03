4892dbec6d616f90d05ffd8d4139650edf7c2440
DJ Steinmetz <dsteinmetz@four51.com>
2020-11-03 14:06:37 -0600

    Add Precor script

--- src/headstart/supplier-product-xp-Facet.ts
+++ src/headstart/supplier-product-xp-Facet.ts
@@ -194,6 +194,38 @@ async function runTorqueFitness() {
     console.log("done")
 }
 
+async function runPrecor() {
+    const creds = config.prod.SEB.Precor;
+    const sdk = await helpers.ocClient(creds.clientID, creds.clientSecret, 'Production');
+    const patch = { xp: { Facets: { supplier: ['Precor']} } };
+
+    console.log("getting products...")
+    const products = await helpers.listAll<Product>(sdk.Products.List);
+    console.log("Got all products")
+
+    const total = products.length;
+    let progress = 0;
+    const errors = {};
+
+    console.log(`Patching ${total} products.`)
+
+    await helpers.batchOperations(products, async function singleOperation(
+        product: Product
+    ): Promise<any> {
+        try {
+            await sdk.Products.Patch(product.ID!, patch);
+            console.log(`Patched ${progress} out of ${total}`);
+            progress++;
+        } catch (e) {
+            console.log("error")
+            errors[product.ID!] = e;
+        }
+    })
+    await helpers.log(errors)
+    helpers.log(errors, 'SEB-patch-errors');
+    console.log("done")
+}
+
 // invoke the proper function here, then debug in the debug panel
 // TODO: Make sure that you add 'ProductAdmin' to the roles array in the helpers.ocClient function
 // in order to avoid authentication errors.  Suppliers do not have full access.
